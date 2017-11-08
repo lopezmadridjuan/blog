a0fe2882e67a99ad3991e69f14f7659407e6404c
KeniaP <perezkeniaa@gmail.com>
2017-11-07 17:15:22 -0800

    terminado

--- djmod/dj_mod/blog/models.py
+++ djmod/dj_mod/blog/models.py
@@ -4,9 +4,39 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
 class post(models.Model):
-    activo=models.BooleanField(default=True)
-    titulo=models.CharField(max_length=45,null=True)
+    activo = models.BooleanField(default = True)
+    titulo= models.CharField(max_length = 45, null = True)
+    mensaje= models.TextField(default="msn")
 
     def __str__(self):
         return str(self.titulo)
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.headline
--- djmod/dj_mod/blog/views.py
+++ djmod/dj_mod/blog/views.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from blog.models  import Blog,Author,Entry
 
 # Create your views here.
+import redis
+def redis_home(request):
+
+    obj= redis.StrictRedis(host='localhost',port=6379,db=0)
+    r= Blog.objects.all(),Author.objects.all(),Entry.objects.all()
+    obj.set("r",r)
+    objset=obj.get("r")
+    context ={
+    "objset":objset
+    }
+    return render(request,"redis.html",context)
